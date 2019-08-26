8a1e27dd03aab59c536617b51ce10169ae5d77ab
FreshyBot <54530562+FreshyBot@users.noreply.github.com>
2019-08-26 10:46:55 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+const botconfig = require("./botconfig.json");
+const Discord = require("discord.js");
+const fs = require("fs");
+const bot = new Discord.Client({disableEveryone: true});
+bot.commands = new Discord.Collection();
+
+fs.readdir("./commands/", (err, files) => {
+
+  if(err) console.log(err);
+  let jsfile = files.filter(f => f.split(".").pop() === "js");
+  if(jsfile.length <= 0){
+    console.log("Nie znaleziono komend");
+    return;
+  }
+
+  jsfile.forEach((f, i) =>{
+    let props = require(`./commands/${f}`);
+    console.log(`${f} loaded!`);
+    bot.commands.set(props.help.name, props);
+  });
+});
+
+bot.on("ready", async () => {
+  bot.user.setActivity("rekrutujemy 8 graczy", {type: "WATCHING"});
+
+});
+
+bot.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
+
+  let prefix = botconfig.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+  let commandfile = bot.commands.get(cmd.slice(prefix.length));
+  if(commandfile) commandfile.run(bot,message,args);
+
+});
+
+bot.login('NjE1MjQ5NzE5MjU3NzI2OTg2.XWLUtw.yoN7_mp3r8dmXH-9XkTfWh_2SV4');
